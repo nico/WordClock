d5422cd83017f7e53a3287bc5d5e1befd8719e40
Nico Weber <nicolasweber@gmx.de>
2014-12-13 18:57:04 -0800

    Timezone observing copypasta.
    Look at all this boilerplate! From
    https://developer.android.com/training/wearables/watch-faces/drawing.html

--- wear/src/main/java/de/amnoid/thakis/wordclock/WordClockService.java
+++ wear/src/main/java/de/amnoid/thakis/wordclock/WordClockService.java
@@ -1,10 +1,17 @@
 package de.amnoid.thakis.wordclock;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.text.format.Time;
 import android.view.SurfaceHolder;
+
+import java.util.TimeZone;
 
 public class WordClockService extends CanvasWatchFaceService {
     public WordClockService() {
@@ -17,10 +24,22 @@ public class WordClockService extends CanvasWatchFaceService {
 
     /* implement service callback methods */
     private class Engine extends CanvasWatchFaceService.Engine {
+        Time mTime;
+        boolean mLowBitAmbient;
+
+        boolean mRegisteredTimeZoneReceiver = false;
+        final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mTime.clear(intent.getStringExtra("time-zone"));
+                mTime.setToNow();
+            }
+        };
 
         @Override
         public void onCreate(SurfaceHolder holder) {
-            /* initialize your watch face */
+            super.onCreate(holder);
+            mTime = new Time();
         }
 
         @Override
@@ -45,7 +64,32 @@ public class WordClockService extends CanvasWatchFaceService {
 
         @Override
         public void onVisibilityChanged(boolean visible) {
-            /* the watch face became visible or invisible */
+            if (visible) {
+                registerReceiver();
+
+                // Update time zone in case it changed while we weren't visible.
+                mTime.clear(TimeZone.getDefault().getID());
+                mTime.setToNow();
+            } else {
+                unregisterReceiver();
+            }
+        }
+
+        private void registerReceiver() {
+            if (mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = true;
+            IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
+            WordClockService.this.registerReceiver(mTimeZoneReceiver, filter);
+        }
+
+        private void unregisterReceiver() {
+            if (!mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = false;
+            WordClockService.this.unregisterReceiver(mTimeZoneReceiver);
         }
     }
 }
